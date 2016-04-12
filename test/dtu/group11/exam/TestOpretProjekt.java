404c3e475c3686ab3ff918536339e232a37873f7
piilmann <christofferpiilmann@Christoffers-MacBook-Pro.local>
2016-04-12 15:15:08 +0200

    TEST: Opret projekt

---
+++ test/dtu/group11/exam/TestOpretProjekt.java
@@ -0,0 +1,16 @@
+package dtu.group11.exam;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestOpretProjekt {
+	
+	public void TestProjekt(){
+		Projekt testprojekt1 = new Projekt(0, "TestProjekt1", "ProjektAnsvarlig1", 1234, 2345);
+	}
+
+}
