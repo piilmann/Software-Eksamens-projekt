6562a54c1e68efc28b681d2b67acdd1b228e2284
piilmann <christofferpiilmann@Christoffers-MacBook-Pro.local>
2016-04-12 12:23:01 +0200

    Projekt variabler

---
+++ src/Projekt.java
@@ -0,0 +1,17 @@
+
+public class Projekt {
+
+	public int projektNummer;
+	public String projektNavn;
+	public String projektLeder;
+	public int startTidspunkt;
+	public int slutTidspunkt;
+
+	public Projekt(int projektNummer, String projektNavn, String projektLeder, int startTidspunkt, int slutTidspunkt) {
+		this.projektNummer = projektNummer;
+		this.projektNavn = projektNavn;
+		this.projektLeder = projektLeder;
+		this.startTidspunkt = startTidspunkt;
+		this.slutTidspunkt = slutTidspunkt;
+	}
+}
